bd54678456f5b39a64ba2a15c29327c8affced81
l1xnan <l1xnan@qq.com>
2024-02-20 11:38:19 +0800

    fix: support python 3.12

--- core/dbt/utils.py
+++ core/dbt/utils.py
@@ -369,3 +369,19 @@ def args_to_dict(args):
 
         dict_args[key] = var_args[key]
     return dict_args
+
+# from https://github.com/python/cpython/blob/3.10/Lib/distutils/util.py#L308
+def strtobool (val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
